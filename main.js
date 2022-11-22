5cad3c03e532cc8fffef4b0a8ad435cac84b0b45
AI-Developer <hassand16@holyportcollege.org.uk>
2022-11-22 14:20:00 +0000

    Initial Commit
    Complete game

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext("2d")
-var width = 16, height = 16
+var width = 20, height = 20
 var bounds = [width, height]
 var snake = [[1,1],[2,1],[3,1]]
 
@@ -12,84 +12,62 @@ var dirs = {
 }
 var dirs_adj = ["a","d","s","w"]
 var last_key
-var last_updated = 0
 
 var playing = false
-var start_key = "enter"
 var move_interval
+var current_dir
 
 function end_game() {
     clearInterval(move_interval)
-    ctx.font = "100px monospace"
-    ctx.fillText("Game over", width/2, height/2)
     playing = false
 }
 
-function random(i,j) {
-    return Math.floor((j-i)*Math.random()) + i
+function random(i,j) {return Math.floor((j-i)*Math.random()) + i}
+
+function visualise(pos, col="#FFFFFF") {
+    var size = canvas.clientWidth;
+    var w = size/width, h = size/height
+    ctx.fillStyle = col
+    let [x,y] = [...pos]
+    ctx.fillRect(x*w,y*w,w,h)
 }
 
 function visualise_snake(snake) {
     var size = canvas.clientWidth;
-    var w = size / width, h = size / height
     ctx.fillStyle = "#000000"
     ctx.fillRect(0,0,size,size)
-
-    ctx.fillStroke = "#FFFFFF"
-    ctx.fillStyle = "#FFFFFF"
-    for (pos of snake) {
-        [x,y] = [...pos]
-        ctx.fillRect(x*w,y*w,w,h)
-    }
-}
-
-function visualise_food(pos) {
-    var size = canvas.clientWidth
-    var w = size / width, h = size / height
-    var [x,y] = [...pos]
-    ctx.fillStyle = "#FF0000"
-    ctx.fillRect(x*w,y*h,w,h)
-}
-
-function eval_arr(arr1,arr2) {
-    if (arr1.length != arr2.length) {return false}
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] != arr2[i]) {return false}
-    } return true
+    for (pos of snake) {visualise((pos), "#FFFFFF")}
 }
 
 var move = NaN
 move = (snake)=>{
     var head = snake[0]
     var next = [...head]
+    current_dir = move_dir
+    // Check if snake is colliding with bounds
     for (let i = 0; i < 2; i++) {
         next[i] += move_dir[i]
         if (next[i] < 0 || next[i] >= bounds[i]) {
-            
             end_game(); return;
         }
     }
-    for (let i = 0; i < snake.length; i++) {
-        hit = true
-        for (n = 0; n < 2; n++) {
-            if (snake[i][n] != next[n]) {
-                hit = false
-            }
-        }
-        if (hit) {
-            clearInterval(move_interval)
-            end_game(); return;
+    // Check if snake colliding with itself
+    for (pos of snake) {
+        if (next[0] == pos[0] && next[1] == pos[1]) {
+            end_game(); return
         }
     }
-    if (eval_arr(next,food)) {
+    // Check if head is colliding with food
+    if (next[0] == food[0] && next[1] == food[1]) {
         food = [random(0,width),random(0,height)]
         snake.push([])
     }
+    // Move rest of snake body along
     for (let i = snake.length-1; i >= 1; i--) {
         let nxt = snake[i-1]
         snake[i] = [...nxt]
     }
-    snake[0] = [...next]
+    snake[0] = next
 }
 
 document.addEventListener("keydown", (ev)=>{
@@ -100,7 +78,7 @@ document.addEventListener("keydown", (ev)=>{
             var invalid
             if (index % 2 == 0) {invalid = dirs_adj[index+1]}
             else {invalid = dirs_adj[index-1]}
-            if (move_dir == dirs[invalid]) {return}
+            if (current_dir == dirs[invalid]) {return}
         }
         move_dir = dirs[key]
     }
@@ -112,8 +90,15 @@ document.addEventListener("keydown", (ev)=>{
         food = [random(0,width),random(0,height)]
         visualise_snake(snake)
         move_interval = setInterval(()=>{
-            move(snake); visualise_snake(snake); visualise_food(food)
-            last_updated = Date.now()
+            move(snake); visualise_snake(snake); visualise(food, "#FF0000")
+            if (!playing) {
+                ctx.font = "15px monospace"
+                ctx.fillStyle = "#FFFFFF"
+                var size = canvas.width
+                ctx.fillText("GAME OVER",30,30)
+            }
         },100)
+        console.log("t")
     };
 })
+visualise_snake(snake)
